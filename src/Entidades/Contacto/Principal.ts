8e5f17af470fbbc59a40c6297a1b1ef5acda7de6
saycardi <saycardi@clb.santillana.com>
2025-01-11 00:11:18 -0500

    Fix: Obtener registro

--- src/Entidades/Contacto/Principal.ts
+++ src/Entidades/Contacto/Principal.ts
@@ -3,14 +3,43 @@ import { Utilidades } from "../../Utilidades/Utilidades";
 export class Principal {
 
     static formContext: Xrm.FormContext;
+    static IdRecord: string;
+    static NameEntity: string;
 
-    public static OnLoad(context: Xrm.Events.EventContext) {
+    public static OnLoad(context: Xrm.Events.EventContext): void {
         try {
             this.formContext = context.getFormContext();
-            console.log(this.formContext);
-            console.log(Utilidades.FormType);
+            this.IdRecord = this.formContext.data.entity.getId();
+            this.NameEntity = this.formContext.data.entity.getEntityName();
+
+            this.ObtenerRegistro(this.NameEntity, this.IdRecord).then((result) => {
+                console.log(result);
+            }).catch((error) => {
+                console.log(error);
+            })
+
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    public static OnSave(): void {
+        try {
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    public static async ObtenerRegistro(entityname: string, entityId: string): Promise<any> {
+        try {
+            if (this.formContext.ui.getFormType() == Utilidades.FormType.Update) {
+                return await Xrm.WebApi.retrieveRecord(entityname, entityId);
+            } else {
+                return null;
+            }
+        } catch (error) {
+
         }
     }
 }
