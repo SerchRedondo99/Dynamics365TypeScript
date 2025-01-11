5f3fa4a3ab9ca4b8c906518116195fecebe31d18
saycardi <saycardi@clb.santillana.com>
2025-01-11 00:22:42 -0500

    Fix : estructura Cuenta/Principal

--- src/Entidades/Cuenta/Principal.ts
+++ src/Entidades/Cuenta/Principal.ts
@@ -1,3 +1,11 @@
-export class Principal {
+export  class Principal {
+
+    public static ObjectoCuenta: Object = new Object();
+    
+    public static OnLoad() { 
+    }
+
+    public static OnSave() { }
+
 
 }
